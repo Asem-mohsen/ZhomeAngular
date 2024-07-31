b9435e5f69e57f6563181ec5cbb2601733974b52
Assem <asemmohsen911@gmail.com>
2024-07-31 13:05:45 +0300

    second push v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,26 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { FooterComponent } from "./footer/footer.component";
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { BrandComponent } from './brand/brand.component';
+import { CartComponent } from './cart/cart.component';
+import { CategoryComponent } from './category/category.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { PlatformComponent } from './platform/platform.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProductComponent } from './product/product.component';
+import { ServiceComponent } from './service/service.component';
+import { ToolComponent } from './tool/tool.component';
+import { ShopComponent } from './shop/shop.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent , HomeComponent , ContactComponent , ShopComponent , AboutComponent , BrandComponent , CartComponent , CategoryComponent , LoginComponent , RegisterComponent , CheckoutComponent , PlatformComponent , ProfileComponent , ProductComponent , ServiceComponent, ToolComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
