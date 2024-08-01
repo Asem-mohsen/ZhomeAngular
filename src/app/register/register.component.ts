303c04b184eb5fa03c09a96622ddc44695a4f8c9
Assem <asemmohsen911@gmail.com>
2024-08-01 18:22:47 +0300

    third push v1

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,7 +6,7 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [RouterLink],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './../login/login.component.css'
 })
 export class RegisterComponent {
 
